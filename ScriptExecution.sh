1405c6136bb6c5772f391793fee63d4ed53ffcc9
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-06-23 00:13:47 +0530

    Update ScriptExecution.sh

--- ScriptExecution.sh
+++ ScriptExecution.sh
@@ -9,3 +9,7 @@ sudo wget https://dl.grafana.com/oss/release/grafana_9.0.0_amd64.deb
 sudo dpkg -i grafana_9.0.0_amd64.deb
 systemctl daemon-reload
 service grafana-server start
+
+
+sudo apt-get install apache2 -y
+
