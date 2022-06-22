54afb52daf54526cf023159c53a7f0a80d819c71
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-06-22 21:53:53 +0530

    Create ScriptExecution.sh

---
+++ ScriptExecution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update 
+cd /tmp
+sudo apt-get install -y adduser libfontconfig1
+sudo wget https://dl.grafana.com/oss/release/grafana_9.0.0_amd64.deb
+sudo dpkg -i grafana_9.0.0_amd64.deb
+systemctl daemon-reload
+service grafana-server start
