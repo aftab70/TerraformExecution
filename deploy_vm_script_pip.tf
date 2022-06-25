60ab091e9fb0a522cb2fcb18d82a77f763924277
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-06-25 20:08:28 +0530

    Update deploy_vm_script_pip.tf

--- deploy_vm_script_pip.tf
+++ deploy_vm_script_pip.tf
@@ -66,6 +66,21 @@ resource "azurerm_network_security_rule" "SSH" {
 
 }
 
+resource "azurerm_network_security_rule" "SSH" {
+  name                        = "HTTP"
+  priority                    = 130
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.Resourse_Group.name
+  network_security_group_name = azurerm_network_security_group.Apache_NSG.name
+
+}
+
 
 
 
