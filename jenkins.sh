a635546582bca6f9609f9c315baeb27012328a3d
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-11-09 13:00:11 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update 
-sudo apt install default-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins -y
-sudo systemctl start jenkins.service
-sudo systemctl enabled jenkins.service
+#sudo apt install default-jdk -y
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+#sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt-get update
+#sudo apt-get install jenkins -y
+#sudo systemctl start jenkins.service
+#sudo systemctl enabled jenkins.service
