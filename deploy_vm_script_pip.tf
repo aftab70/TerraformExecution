a7280165f92f4ac28ffe4481222a41a5ed6ce776
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-11-09 13:51:48 +0530

    Update deploy_vm_script_pip.tf

--- deploy_vm_script_pip.tf
+++ deploy_vm_script_pip.tf
@@ -186,7 +186,7 @@ resource "azurerm_virtual_machine" "WebApp" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
